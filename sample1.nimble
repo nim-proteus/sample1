b5e761dde56c1821e541cc349a32a59e9a762695
cpersona <cpersona6@gmail.com>
2022-12-02 22:38:49 -0500

    Basic window opens and closes

---
+++ sample1.nimble
@@ -0,0 +1,15 @@
+# Package
+
+version       = "0.1.0"
+author        = "cpersona"
+description   = "Sample of Nim-Proteus simulation"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["sample1"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.6"
+requires "https://github.com/nim-proteus/ecs"
+requires "https://github.com/nim-proteus/graphics"
---
+++ src/sample1.nim
@@ -0,0 +1,25 @@
+# This is just an example to get you started. A typical binary package
+# uses this file as the main entry point of the application.
+
+import graphics
+import graphics/oglrenderer
+import std/logging
+
+when isMainModule:
+    let fmtStr = "[$time] - $levelname: "
+    addHandler(newConsoleLogger(fmtStr = fmtStr))
+    # addHandler(newRollingFileLogger(filename = "log.txt", fmtStr = fmtStr))
+
+    var g = newGraphics(newOglRenderer())
+    g.openWindow(640, 480, "Sample")
+
+    # var path = "some/path/to/model.xyz"
+    # discard g.loadModel(path)
+    # var mi = g.getModelInstance(path)
+
+    var tasks = newSeq[RenderTask]()
+    # for mesh in mi.meshes:
+    #     tasks.add(RenderTask(mode: RenderMode.Projection, modelId: mi.id, meshId: mesh.meshId))
+
+    while g.isRunning():
+        g.render(tasks)
